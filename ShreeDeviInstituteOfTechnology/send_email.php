3e0476ec0157334393bc58e57bd0cb96b99860e6
Vijendra Singh <42693365+vijendrashivaansh@users.noreply.github.com>
2018-11-10 22:44:53 +0530

    Update send_email.php

--- ShreeDeviInstituteOfTechnology/send_email.php
+++ ShreeDeviInstituteOfTechnology/send_email.php
@@ -1,27 +1,29 @@
 <?php
-$from = '<vijendrasingh221996@gmail.com>';
-$to = '<vijendrashivaansh227@gmail.com>';
-$subject = 'Hi!';
-$body = "Hi,\n\nHow are you?";
+$mailTo = 'vijendrasingh221996@gmail.com';
+$mailSub = $_POST('mail_sub');
+$mailMsg = $_POST('mail_msg');
 
-$headers = array(
-    'From' => $from,
-    'To' => $to,
-    'Subject' => $subject
-);
+require 'PHPMailer-master/PHPMailerAutoload.php';
+$mail = new PHPMailer();
+$mail -> IsSmtp();
+$mail -> SMTPDebug = 1;
+$mail -> SMTPAuth = true;
+$mail -> SMTPSecure = 'ss1';
+$mail -> Host = "smtp.gmail.com";
+$mail -> Port = 465;
+$mail -> IsHTML(true);
+$mail -> Username = "demoemailaddressSDIT@yahoo.com";
+$mail -> Password = "demoemailpasswordSDIT";
+$mail -> SetFrom("demoemailaddressSDIT@yahoo.com");
 
-$smtp = Mail::factory('smtp', array(
-    'host' => 'ssl://smtp.gmail.com',
-    'port' => '465',
-    'auth' => true,
-    'username' => 'johndoe@gmail.com',
-    'password' => 'passwordxxx'
-));
+$mail -> Subject = $mailSub;
+$mail -> Body = $mailMsg;
+$mail -> AddAddress($mailto);
 
-$mail = $smtp->send($to, $headers, $body);
-
-if (PEAR::isError($mail)) {
-    echo('<p>' . $mail->getMessage() . '</p>');
-} else {
-    echo('<p>Message successfully sent!</p>');
+if(!$mail -> Send())
+{
+    echo "Mail Not Sent";
+}
+else {
+    echo "Mail Sent";
 }
